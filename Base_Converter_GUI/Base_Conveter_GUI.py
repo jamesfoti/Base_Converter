8f768ad509d7251a76475cc091664dbe159f3fd2
James Foti <jameswilliamfoti@gmail.com>
2019-12-30 16:17:26 -0800

    Created a new starting point. I am going to model: https://www.rapidtables.com/convert/number/base-converter.html

---
+++ Base_Converter_GUI/Base_Conveter_GUI.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# Create & Configure root
+root = Tk()
+root.title("Base Converter")
+root.geometry("400x500")
+root.configure(background = 'grey')
+
+
+
+
+root.mainloop()
